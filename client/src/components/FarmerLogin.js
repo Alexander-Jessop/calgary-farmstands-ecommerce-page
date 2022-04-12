3d099ec09455bbf9b0cd4767089ed9ec43ffbe08
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-12 17:27:34 -0600

    Adding function that prevents reload of browser when pressing sign in

--- client/src/components/FarmerLogin.js
+++ client/src/components/FarmerLogin.js
@@ -4,49 +4,72 @@ const FarmerLogin = () => {
 
   const [farmer, setFarmer] = useState('');
   const [pwd, setPwd] = useState('');
+  //Just for learning purposes - to be removed
+  const [success, setSuccess] = useState(false);
 
-    return (
-      <section>
-        <h1>Sign In</h1>
-        <form>
-          <label htmlFor="username">
-            Username:
-            <input
-              type="text"
-              id="username"
-              autoComplete="off"
-              onChange={(e) => setFarmer(e.target.value)}
-              value={farmer}
-              required
-            />
-          </label>
-          <br />
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
-              required
-            />
-          </label>
-          <br />
-          <label htmlFor="submit">
-            <input type="submit" id="submit" value="Sign In" />
-          </label>
-        </form>
-        <p>
-          Need an account?
-          <br />
-          <a href="#">Sign Up</a>
-          <br />
-          <a href="#">Continue as guest</a>
-          <br />
-          <a href="#">Forgot Password</a>
-        </p>
-      </section>
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(farmer, pwd);
+    setFarmer('');
+    setPwd('');
+    //Placeholder to see action of form submission
+    setSuccess(true);
   }
+
+  return (
+    <div>
+      {success ? (
+        <section>
+          <h1>You are logged in</h1>
+          <br />
+          <p>
+            <a href="#">Go to home page</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">
+              Username:
+              <input
+                type="text"
+                id="username"
+                autoComplete="off"
+                onChange={(e) => setFarmer(e.target.value)}
+                value={farmer}
+                required
+              />
+            </label>
+            <br />
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+            </label>
+            <br />
+            <label htmlFor="submit">
+              <input type="submit" id="submit" value="Sign In" />
+            </label>
+          </form>
+          <p>
+            Need an account?
+            <br />
+            <a href="#">Sign Up</a>
+            <br />
+            <a href="#">Continue as guest</a>
+            <br />
+            <a href="#">Forgot Password</a>
+          </p>
+        </section>
+      )}
+    </div> 
+  );
+}
 
 export default FarmerLogin
