7d03def47f3c54a8ce6cbe3da1f11c71336fae03
Neetha <neethacohort@gmail.com>
2022-04-12 14:03:11 -0600

    NavBar buttons added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ApiData from "./components/ApiData";
 
-const App = () => {
-  return <ApiData />;
-};
+import Navbar from "./components/Navbar/Navindex";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={"Hello Team"}></Route>
+          <Route path="/about" element={"Farm"}></Route>
+          <Route path="/contact-us" element={"Information"}></Route>
+          <Route path="/location" element={<ApiData />}></Route>
+          <Route path="/sign-up" element={"Sign Up Form"}></Route>
+          <Route path="/sign-in" element={"Sign In Form"}></Route>
+          <Route path="/cart" element={"Details"}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
 
 export default App;
