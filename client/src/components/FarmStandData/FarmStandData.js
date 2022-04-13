ce20615fd8d760ef282381fc46ed5d88f71509ab
Alexander Jessop <alexanderjessop@protonmail.com>
2022-04-12 23:53:09 -0600

    Add more styling to Farmstand cards - currently too much negitive space

--- client/src/components/FarmStandData/FarmStandData.js
+++ client/src/components/FarmStandData/FarmStandData.js
@@ -12,7 +12,6 @@ const ApiData = (props) => {
   useEffect(() => {
     axios.get("/api").then((res) => {
       setFarmStandData(res.data);
-      console.log(res.data);
     });
   }, []);
 
@@ -21,19 +20,22 @@ const ApiData = (props) => {
       <main>
         {farmStandData.map((farmStand) => (
           <div className="farmDataDiv" key={Math.random().toString()}>
-            <h1>
-              {farmStand.community} {farmStand.vendor_name}
-            </h1>
             <img
               width={100}
               height={100}
-              src="https://showscene.ca/wp-content/uploads/2017/03/1c0d0f0cb8b7f2fb2685da9798efe42b_big-image-png-image-placeholder-clipart_2400-2400.png"
+              src="https://images.unsplash.com/photo-1495107334309-fcf20504a5ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               alt="placeholder"
             />
+            <h1>
+              {farmStand.community}
+              {/* {farmStand.vendor_name} */}
+            </h1>
+            <hr />
             <address>{farmStand.address}</address>
             <Season season={farmStand.duration} />
             <RefindedHours hours={farmStand.hours} />
-            <ShortenData data={farmStand.vendor_description} />
+            {/* dog ear tag to flip card to show data */}
+            {/* <ShortenData data={farmStand.vendor_description} /> */}
           </div>
         ))}
       </main>
