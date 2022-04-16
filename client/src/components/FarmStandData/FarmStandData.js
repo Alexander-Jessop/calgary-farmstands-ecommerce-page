b4421a61e613fe3dbc90ac688ea594dd13a55192
Alexander Jessop <alexanderjessop@protonmail.com>
2022-04-16 14:57:34 -0600

    Changed .css to .module.css prevent style carryover

--- client/src/components/FarmStandData/FarmStandData.js
+++ client/src/components/FarmStandData/FarmStandData.js
@@ -7,7 +7,8 @@ import FavoriteButton from "../UI/FavoriteButton";
 import LocationTxt from "../UI/LocationTxt";
 import Phone from "../UI/Phone";
 import Email from "../UI/Email";
-import "./FarmStandData.css";
+import StoreFront from "../UI/StoreFront";
+import classes from "./FarmStandData.module.css";
 
 const FarmStandData = (props) => {
   const [farmStandData, setFarmStandData] = useState([]);
@@ -28,26 +29,30 @@ const FarmStandData = (props) => {
   return (
     <main>
       {farmStandData.map((farmStand) => (
-        <div className="farmDataDiv" key={farmStand.id}>
+        <div className={classes.farmDataDiv} key={farmStand.id}>
           <img
             width={100}
             height={100}
             src={farmStand.images}
             alt={farmStand.vendor_name}
           />
-          <h1 onClick={() => selectedFarmStand(farmStand.id)}>
+          <h1
+            style={{ cursor: "pointer" }}
+            onClick={() => selectedFarmStand(farmStand.id)}
+          >
             {farmStand.community}{" "}
           </h1>
-          <hr />
-          <address>{farmStand.address}</address>
+          <hr className={classes.hr} />
+          <address className={classes.address}>{farmStand.address}</address>
           <Season season={farmStand.duration} />
           <RefindedHours hours={farmStand.hours} />
           {/* dog ear tag to flip card to show data */}
           {/* <ShortenData data={farmStand.vendor_description} /> */}
-          <div className="heartpos">
+          <div className={classes.cardButton}>
             <LocationTxt />
             <Phone />
             <Email />
+            <StoreFront />
             <FavoriteButton />
           </div>
         </div>
---
+++ client/src/components/UI/StoreFront.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from "react";
+import { FaStore } from "react-icons/fa";
+
+function StoreFront(props) {
+  return (
+    <div>
+      <FaStore />
+    </div>
+  );
+}
+
+export default StoreFront;
