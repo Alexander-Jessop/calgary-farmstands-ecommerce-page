39e7f10a014a62825894d20ee11bb73f5d25eaff
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-19 22:57:37 -0600

    Added to farmer schema

--- server/db/model/farmerModel.js
+++ server/db/model/farmerModel.js
@@ -2,35 +2,61 @@ const mongoose = require("../mongoose");
 const { Schema } = mongoose;
 
 const farmerSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  password: String,
-  phone: Number,
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
     lowercase: true,
+    unique: true,
   },
-  businessname: String,
-  address: addressSchema,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs: Number,
+  businessname: {
+    type: String,
+    required: true,
+    unique: true,
   },
+  address: {
+    street: String,
+    city: String,
+    province: String,
+    postalcode: String,
+  },
+  // comments: [{ body: String, date: Date }],
+  // date: { type: Date, default: Date.now },
+  // hidden: Boolean,
+  // meta: {
+  //   votes: Number,
+  //   favs: Number,
+  // },
 });
 
-const FarmerModel = model("Farmer", farmerSchema);
 
-const createFarmerModel = async (farmerModel) => {
-  const newFarmerModel = await FarmerModel.create(farmerModel);
-  return newFarmerModel;
-};
 
-const getAllFarmerModels = async () => {
-  const farmerModels = await FarmerModel.find();
-  return farmerModels;
-};
+// const createFarmerModel = async (farmerModel) => {
+//   const newFarmerModel = await FarmerModel.create(farmerModel);
+//   return newFarmerModel;
+// };
 
-module.exports = { getAllFarmerModels, createFarmerModel };
+// const getAllFarmerModels = async () => {
+//   const farmerModels = await FarmerModel.find();
+//   return farmerModels;
+// };
+
+const FarmerModel = mongoose.model("Farmer", farmerSchema);
+
+module.exports =  FarmerModel;
