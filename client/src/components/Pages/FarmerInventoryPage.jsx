31fe27c486a19ab48cf7b444880ed98c3d140b07
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-22 13:05:15 -0600

    Added a page which renders Farmer inventory

---
+++ client/src/components/Pages/FarmerInventoryPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Inventory from '../FarmerInventory/Inventory'
+
+const FarmerInventoryPage = () => {
+  return (
+      <React.Fragment>
+        <Inventory />
+      </React.Fragment>
+    
+  )
+}
+
+export default FarmerInventoryPage
