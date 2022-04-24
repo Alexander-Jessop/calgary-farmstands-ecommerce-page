11fa51ea3787fe10553e2e66766c535051c08f23
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-24 08:12:22 -0600

    Created section showing FarmStand Information

---
+++ client/src/components/FarmerInventory/FarmStandDetails.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "./FarmStandInfo.css";
+
+const FarmStandDetails = (props) => {
+  const id = props.id;
+  const [standInfo, setStandInfo] = useState();
+
+  useEffect(() => {
+    const farmStandInfo = async () => {
+      let response = await fetch(`/inventory/${id}`);
+      let data = await response.json();
+      setStandInfo(data);
+    };
+
+    farmStandInfo();
+  }, [id]);
+
+  if (!standInfo) {
+    return <p>Loading data...</p>;
+  }
+  return (
+    <div className="info">
+      <label>FarmStand id: </label>
+      <span>{standInfo._id}</span>
+      <br />
+      <label>Name: </label>
+      <span>{standInfo.vendor_name}</span>
+      <br />
+      <label>Hours: </label>
+      <span>{standInfo.hours}</span>
+      <br />
+      <label>Products: </label>
+      <span>{standInfo.product_description}</span>
+    </div>
+  );
+};
+
+export default FarmStandDetails;
