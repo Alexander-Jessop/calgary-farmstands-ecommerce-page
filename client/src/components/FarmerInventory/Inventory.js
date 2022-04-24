5065fe2e7f3d2cdb7305e0c80d1870d237ea8a55
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-24 08:15:27 -0600

    Imported FarmStandDetail file for viewing on inventory page: Will change hard-coded value

--- client/src/components/FarmerInventory/Inventory.js
+++ client/src/components/FarmerInventory/Inventory.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import FarmStandDetails from './FarmStandDetails'
 
 const Inventory = () => {
   return (
-    <div>Inventory</div>
+    <div>
+      <FarmStandDetails id={"626372742a678c99fded8d0d"}/>
+    </div>
   )
 }
 
