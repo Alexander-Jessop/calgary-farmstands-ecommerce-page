905d5542423313312bf2ae1e821c1929e7554dbd
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-04-27 14:43:24 -0600

    Created button, when pressed opens up form

--- client/src/components/FarmerInventory/Inventory.js
+++ client/src/components/FarmerInventory/Inventory.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FarmStandDetails from './FarmStandDetails'
+import InventoryCreationForm from './InventoryCreationForm'
 
 const Inventory = () => {
+  const [popUpForm, setPopUpForm] = useState(false);
+  
+
   return (
     <div>
-      <FarmStandDetails id={"626372742a678c99fded8d0d"}/>
+      <FarmStandDetails id={"626372742a678c99fded8d0d"} />
+      <button onClick={()=>{setPopUpForm(true)}}>Create and edit Inventory</button>
+      {popUpForm && <InventoryCreationForm setPopUpForm={setPopUpForm} />}
     </div>
   )
 }
